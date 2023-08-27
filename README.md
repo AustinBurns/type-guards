330e7fa791c18d6d7c6da438ef600bb47bfd7d7e
Austin Burns <austin.c.burns@gmail.com>
2023-08-27 12:01:53 -0400

    Add initial type guards

---
+++ README.md
@@ -0,0 +1,1 @@
+Type guards that I commonly use
---
+++ misc.ts
@@ -0,0 +1,6 @@
+export const isFunction = (x: unknown): x is (...args: unknown[]) => unknown =>
+  typeof x === 'function';
+
+export const isVoid = (x: unknown): x is void => x === void 0;
+
+export const isError = (error: unknown): error is Error => error instanceof Error;
